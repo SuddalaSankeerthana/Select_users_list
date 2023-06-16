122187ce53950ec503c823b50c8bc27ad4778e62
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-06-16 11:49:49 +0530

    created different files for different components and data

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import UserData, { UsersData } from "./UserData";
-export const users: UserData[] = [
-  {
-    id: 1,
-    name: "user1",
-    role: "Developer",
-    gmail: "user1@everesr.com",
-    select: null,
-  },
-  {
-    id: 2,
-    name: "user2",
-    role: "Team lead",
-    gmail: "user2@everesr.com",
-    select: null,
-  },
-  {
-    id: 1,
-    name: "user3",
-    role: "Developer",
-    gmail: "user3@everesr.com",
-    select: null,
-  },
-  {
-    id: 1,
-    name: "user4",
-    role: "Developer",
-    gmail: "user4@everesr.com",
-    select: null,
-  },
-];
+import { UsersDataComponent } from "./UserDataComponet";
+import { users } from "./Data";
 function App() {
   return (
     <section>
       <div className="App">
         <h1>Invite users</h1>
         <p>At least 1 member needs to be selected</p>
-        <UsersData users={users} />
+        <UsersDataComponent users={users} />
         <button id="cancel">Cancel</button>
         <button id="submit">Submit</button>
       </div>
---
+++ src/Data.tsx
@@ -0,0 +1,31 @@
+import UserDataInterface from "./Interfaces";
+export const users: UserDataInterface[] = [
+    {
+      id: 1,
+      name: "user1",
+      role: "Developer",
+      gmail: "user1@everesr.com",
+      select: false,
+    },
+    {
+      id: 2,
+      name: "user2",
+      role: "Team lead",
+      gmail: "user2@everesr.com",
+      select: false,
+    },
+    {
+      id: 1,
+      name: "user3",
+      role: "Developer",
+      gmail: "user3@everesr.com",
+      select: false,
+    },
+    {
+      id: 1,
+      name: "user4",
+      role: "Developer",
+      gmail: "user4@everesr.com",
+      select: false,
+    },
+  ];
