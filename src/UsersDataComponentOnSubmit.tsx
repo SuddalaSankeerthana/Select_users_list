4d2ffb9bfec6a94ad47a6a22fdb227bacb7acf27
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-06-18 01:14:42 +0530

    Added code for adding userDatacomponets on submit and on add

---
+++ src/UsersDataComponentOnSubmit.tsx
@@ -0,0 +1,18 @@
+import UserDataInterface from "./Interfaces";
+import  React ,{ useState } from "react";
+export function UsersDataComponentOnSubmit({
+  user,
+}: {
+  user: UserDataInterface;
+}): React.ReactElement {
+  const [select, setSelect] = useState(user.select);
+  return (
+    <table>
+      <tr>
+        <td> {user.name}</td>
+        <td> {user.role}</td>
+        <td> {user.gmail}</td>
+      </tr>
+    </table>
+  );
+}
