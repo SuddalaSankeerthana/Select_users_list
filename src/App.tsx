3802855b22a69c1618667660264e2b370da67967
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-06-18 01:21:12 +0530

    Added clickhandle function to handle checkbox clicked and addList function to update data on submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import "./App.css";
-import { UsersDataComponent } from "./UserDataComponet";
+import {
+  UsersDataComponentOnAdd,
+  UsersDataComponentOnSubmit,
+} from "./UserDataComponet";
 import { users } from "./Data";
+import { useState } from "react";
+import UserDataInterface from "./Interfaces";
 function App() {
+  const [add, setAdd] = useState(false);
+  const [submit, setSubmit] = useState(false);
+  const userdata = users.filter((ele) => ele.select === true);
+  const [userData, setUserData] = useState(userdata);
+  const addedList = () => {
+    setUserData(userData.filter((ele) => ele.select === true));
+    console.log(userData);
+  };
+  const clickHandler = (id: number) => {
+    users.filter((ele) => {
+      if (ele.id === id) {
+        return (ele.select = !ele.select);
+      }
+    });
+    console.log(users);
+  };
   return (
     <section>
       <div className="App">
         <h1>Invite users</h1>
-        <p>At least 1 member needs to be selected</p>
-        <UsersDataComponent users={users} />
-        <button id="cancel">Cancel</button>
-        <button id="submit">Submit</button>
-      </div>
-    </section>
-  )
-}
+        <button
+          id="Add"></button>
+          </div>
+         </section> )
 
 export default App;
